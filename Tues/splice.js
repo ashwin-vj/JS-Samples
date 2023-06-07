e5e7d625a05b88e92b3dee9448443101ae43d357
ashwin-vj <ashwinvijay108@gmail.com>
2023-06-07 10:10:43 +0530

    JS

---
+++ Tues/splice.js
@@ -0,0 +1,7 @@
+var numbers = [1, 2, 3, 4, 5];
+numbers.splice(2, 2, 6, 7);
+console.log(numbers);       // Output: [1, 2, 6, 7, 5]
+
+
+
+//Syntax:  array.splice(start, deleteCount, item1, item2, ...);
